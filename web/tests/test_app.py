f92a5932155dcdd0f7c656eac58fe634fe25ae13
Tom <tomwight233@gmail.com>
2025-09-12 16:42:42 +0100

    again

--- web/tests/test_app.py
+++ web/tests/test_app.py
@@ -1,7 +1,7 @@
 def test_get_home(web_client):
     response = web_client.get("/hello")
     assert response.status_code == 200
-    assert response.data.decode("utf-8") == "Hello, world!"
+    assert response.data.decode("utf-8") == "Hello, world!!"
     
 def test_get_first_message(web_client):
     post_response = web_client.post("/", data = {"message": "first message"})
